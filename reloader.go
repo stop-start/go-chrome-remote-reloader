68a826373cf32d26191edbb03bc52cd8caedde71
elinor <stop.start.dev@gmail.com>
2018-09-28 22:38:49 +0300

    Refactoring and bug fixes

--- reloader.go
+++ reloader.go
@@ -16,7 +16,9 @@ import (
 type RemoteConfig struct {
 	// ExecName is the name of the chrome executable (e.g "google-chrome", "chromium")
 	ExecName string
-	// DebugPort is the chrome remote debugging port, usually 9222
+	// DebugAddr is the chrome remote debugging address (default localhost)
+	DebugAddr string
+	// DebugPort is the chrome remote debugging port (default 9222)
 	DebugPort int
 	// UserDataDir is the folder for chrome user profile
 	UserDataDir string
@@ -53,6 +55,7 @@ type parameters struct {
 func NewRemoteConfig() *RemoteConfig {
 	return &RemoteConfig{
 		ExecName:    "google-chrome",
+		DebugAddr:   "localhost",
 		DebugPort:   9222,
 		UserDataDir: "/tmp/.chrome-remote-profile",
 		Addr:        "localhost",
@@ -67,7 +70,7 @@ func (rc *RemoteConfig) RemoteChrome() (context.CancelFunc, error) {
 	cmd := exec.CommandContext(
 		ctx,
 		rc.ExecName,
-		fmt.Sprintf("--remote-debugging-port=%d", rc.Port),
+		fmt.Sprintf("--remote-debugging-port=%d", rc.DebugPort),
 		fmt.Sprintf("--user-data-dir=%s", rc.UserDataDir),
 		fmt.Sprintf("http://%s:%d%s", rc.Addr, rc.Port, rc.Route),
 	)
@@ -82,12 +85,14 @@ func (rc *RemoteConfig) ReloadAllTabs() error {
 
 // ReloadTab reloads one chrome tab by checking if the tab URL has route as suffix
 func (rc *RemoteConfig) ReloadTab(route string) error {
-	return reloadTabs(rc.Addr, rc.DebugPort, strings.HasSuffix, route)
+	return reloadTabs(rc.DebugAddr, rc.DebugPort, strings.HasSuffix, route)
 }
 
 // ReloadTabGroup reloads a group of chrome tabs by checking if the tab URL contains the subroute
 func (rc *RemoteConfig) ReloadTabGroup(subroute string) error {
-	return reloadTabs(rc.Addr, rc.DebugPort, strings.Contains, subroute)
+	return reloadTabs(rc.DebugAddr, rc.DebugPort, strings.Contains, subroute)
+}
+
 }
 
 func reloadTabs(addr string, port int, conditionFunc func(string, string) bool, route string) error {
